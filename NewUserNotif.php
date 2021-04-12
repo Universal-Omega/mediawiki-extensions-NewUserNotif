aed75460b7f39286353253c72858b8958bff1d70
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-04-12 15:13:31 -0600

    Converted to ExtensionRegistry

--- NewUserNotif.php
+++ NewUserNotif.php
@@ -1,44 +1,16 @@
 <?php
-if ( ! defined( 'MEDIAWIKI' ) ) {
-    die();
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'NewUserNotif' );
+
+	$wgMessagesDirs['NewUserNotif'] = __DIR__ . '/i18n';
+
+	wfWarn(
+		'Deprecated PHP entry point used for the New User Email Notification extension. ' .
+		'Please use wfLoadExtension() instead, ' .
+		'see https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Extension_registration for more details.'
+	);
+
+	return;
+} else {
+	die( 'This version of the New User Email Notification extension requires MediaWiki 1.35+' );
 }
-
-/**
- * Extension to provide customisable email notification of new user creation
- *
- * @file
- * @author Rob Church <robchur@gmail.com>
- * @ingroup Extensions
- * @copyright © 2006 Rob Church
- * @license GNU General Public Licence 2.0 or later
- */
-
-$wgExtensionCredits['other'][] = [
-	'path' => __FILE__,
-	'name'           => 'New User Email Notification',
-	'version'        => '1.6.0',
-	'author'         => 'Rob Church',
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:New_User_Email_Notification',
-	'descriptionmsg' => 'newusernotif-desc',
-];
-
-$dir = dirname(__FILE__) . '/';
-$wgMessagesDirs['NewUserNotif'] = __DIR__ . '/i18n';
-$wgAutoloadClasses['NewUserNotifier'] = $dir . 'NewUserNotif.class.php';
-
-$wgHooks['LocalUserCreated'][] = 'NewUserNotifier::onLocalUserCreated';
-
-/**
- * Email address to use as the sender
- */
-$wgNewUserNotifSender = $wgPasswordSender;
-
-/**
- * Users who should receive notification mails
- */
-$wgNewUserNotifTargets[] = 1;
-
-/**
- * Additional email addresses to send mails to
- */
-$wgNewUserNotifEmailTargets = [];
